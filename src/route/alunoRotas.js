87ff457c4694b6c66967ad3c472c98124a0d66ca
Dryego <dryego@MacBook-Air-de-Diogo.local>
2023-10-31 12:19:56 -0300

    correcao na rota.

--- src/route/alunoRotas.js
+++ src/route/alunoRotas.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const novoAluno = require("../controller/cadastroAluno");
 const detalharAluno = require("../controller/detalharAluno");
-const editaAluno = require("../controller/editaraluno");
+
 const excluirAluno = require("../controller/excluirAluno");
 const validarEsquemas = require("../middlewares/validarEsquemas");
 const esquemaAluno = require("../schemes/esquemaAluno");
@@ -11,7 +11,7 @@ const rotas = express.Router();
 
 rotas.post("/cadastro", validarEsquemas(esquemaAluno), novoAluno);
 rotas.get("/detalhar/:id", detalharAluno);
-rotas.put("/editar/:id", validarEsquemas(esquemaAluno), editaAluno);
+rotas.put("/editar/:id", validarEsquemas(esquemaAluno));
 rotas.delete("/excluir/:id", excluirAluno);
 rotas.get("/exibir", exibirAlunos);
 
