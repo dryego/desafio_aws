ec450bd9d654ad2cfdea0aed73af85b9311ccb21
Dryego <dryego@MacBook-Air-de-Diogo.local>
2023-10-31 11:52:58 -0300

    editar aluno.

---
+++ src/controller/editarAluno.js
@@ -0,0 +1,24 @@
+const knex = require("../connections/kenx");
+const { buscarAlunoID } = require("../util/funcaoBusca");
+
+const editaAluno = async (req, res) => {
+  const { id } = req.params;
+  const { nome, idade, nota1, nota2, professor, sala } = req.body;
+
+  try {
+    if ((await buscarAlunoID(id)) === undefined) {
+      return res.status(400).json({ messagem: "Aluno não localizado." });
+    }
+
+    await knex("alunos")
+      .update({ nome, idade, nota1, nota2, professor, sala })
+      .where({ id });
+
+    return res.json({ menssagem: "Dados atualizado com sucesso!" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
+};
+
+module.exports = editaAluno;
--- src/route/alunoRotas.js
+++ src/route/alunoRotas.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const novoAluno = require("../controller/cadastroAluno");
 const detalharAluno = require("../controller/detalharAluno");
+const editaAluno = require("../controller/editaraluno");
 // const excluirAluno = require("../controller/excluirAluno");
-// const editaAluno = require("../controller/editarAluno");
 const validarEsquemas = require("../middlewares/validarEsquemas");
 const esquemaAluno = require("../schemes/esquemaAluno");
 
@@ -10,7 +10,7 @@ const rotas = express.Router();
 
 rotas.post("/cadastro", validarEsquemas(esquemaAluno), novoAluno);
 rotas.get("/detalhar/:id", detalharAluno);
-// rotas.put("/editar/:id", validarEsquemas(esquemaAluno), editaAluno);
+rotas.put("/editar/:id", validarEsquemas(esquemaAluno), editaAluno);
 // rotas.delete("/excluir/:id", excluirAluno);
 
 module.exports = rotas;
