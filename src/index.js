6735f826a49c17970cf60caa57ca4ca8cc06f9e9
Dryego <dryego@MacBook-Air-de-Diogo.local>
2023-10-31 17:59:06 -0300

    documentacao

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const alunoRotas = require("./route/alunoRotas");
-//const swaggerUi = require("swagger-ui-express");
-//const swaggerDocs = require("./util/swagger.json");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocs = require("./document/swagger.json");
 
 const app = express();
 
 app.use(express.json());
-//app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use("/aluno", alunoRotas);
 
